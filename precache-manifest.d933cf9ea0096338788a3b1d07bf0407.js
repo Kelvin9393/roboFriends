afb9b879eeffd27b362e415f4790c08b73acf08a
Kelvin <kelvinlssiang9393@hotmail.com>
2019-01-21 21:24:20 +0800

    Updates

---
+++ precache-manifest.d933cf9ea0096338788a3b1d07bf0407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1a95c78c8b1e07287e07",
+    "url": "/roboFriends/static/js/runtime~main.1a95c78c.js"
+  },
+  {
+    "revision": "4279f81df3418849e373",
+    "url": "/roboFriends/static/js/main.4279f81d.chunk.js"
+  },
+  {
+    "revision": "bd94d134a545ad153c59",
+    "url": "/roboFriends/static/js/1.bd94d134.chunk.js"
+  },
+  {
+    "revision": "4279f81df3418849e373",
+    "url": "/roboFriends/static/css/main.f11b0281.chunk.css"
+  },
+  {
+    "revision": "bd94d134a545ad153c59",
+    "url": "/roboFriends/static/css/1.d9dab7a0.chunk.css"
+  },
+  {
+    "revision": "16295f62d38475295e17bf60d6d178b1",
+    "url": "/roboFriends/index.html"
+  }
+];
